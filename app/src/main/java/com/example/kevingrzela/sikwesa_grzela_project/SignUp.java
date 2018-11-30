e32e99440152ee7c5aede82b50de7a22ac6360e4
sikwesa <nyakiwe@hotmail.com>
2018-11-30 17:17:43 -0500

    testing

---
+++ app/src/main/java/com/example/kevingrzela/sikwesa_grzela_project/SignUp.java
@@ -0,0 +1,13 @@
+package com.example.kevingrzela.sikwesa_grzela_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
